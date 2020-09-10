5a522ebaed75856bd2058ea6c2164688e07d67f5
SepSol <SR-75@live.com>
2020-09-10 01:47:15 -0700

    Updates

---
+++ precache-manifest.37ca22007588bb8d0b723fcb9e2780ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c968e622f60527df5ba3b3d0248e3b53",
+    "url": "/todo-react-ts/index.html"
+  },
+  {
+    "revision": "83456efe2d01733ea2f6",
+    "url": "/todo-react-ts/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6d6988c06df6351ed402",
+    "url": "/todo-react-ts/static/js/2.de42b8c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react-ts/static/js/2.de42b8c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83456efe2d01733ea2f6",
+    "url": "/todo-react-ts/static/js/main.7d03ed2b.chunk.js"
+  },
+  {
+    "revision": "ef39a1be74c761457387",
+    "url": "/todo-react-ts/static/js/runtime-main.3e9c5c88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-react-ts/static/media/logo.5d5d9eef.svg"
+  }
+]);
