e49622dd25a0b6f9c9b2e0ff24bf285641d1cb3a
SepSol <SR-75@live.com>
2020-09-10 05:05:58 -0700

    added menu bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import SearchAppBar from './components/SearchAppBar';
 import CheckboxList from './components/CheckboxList';
 
 function App() {
   return (
     <div className="App">
+      <SearchAppBar />
       <CheckboxList />
     </div>
   );
